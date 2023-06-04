0527cb625bba707e323014cd022aa5581a93fac6
Felix Wang <felix-wang@outlook.de>
2023-06-04 20:45:23 +0200

    setup node react babel webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './main.jsx',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.jsx', '.ts', '.js'],
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+}
