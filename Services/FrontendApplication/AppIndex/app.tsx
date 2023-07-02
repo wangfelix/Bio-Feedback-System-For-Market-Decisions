4d24f85e1135096faea9a4e80ca9c90ea1c3bd26
Felix Wang <felix-wang@outlook.de>
2023-07-02 18:54:49 +0200

    implement pages and routes for them

--- Services/FrontendApplication/AppIndex/app.tsx
+++ Services/FrontendApplication/AppIndex/app.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { LandingPage } from "Pages/landingPage";
+import { HistoryPage } from "Pages/historyPage";
+import { LiveFeedPage } from "Pages/liveFeedPage";
+import { AdminPage } from "Pages/adminPage";
+import { SensorsAndDevicesPage } from "Pages/sensorsAndDevicesPage";
+import { Paths } from "Utils/paths";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <LandingPage />
+            <Routes>
+                <Route path={Paths.LANDING_PAGE} Component={LandingPage} />
+
+                <Route path={Paths.HISTORY_PAGE} Component={HistoryPage} />
+
+                <Route path={Paths.LIVE_FEED_PAGE} Component={LiveFeedPage} />
+
+                <Route path={Paths.SENSORS_AND_DEVICES_PAGE} Component={SensorsAndDevicesPage} />
+
+                <Route path={Paths.ADMIN_PAGE} Component={AdminPage} />
+
+                <Route Component={ErrorPage} />
+            </Routes>
         </BrowserRouter>
     );
 };
+
+const ErrorPage = () => <>404</>;
+
 export default App;
---
+++ Services/FrontendApplication/Pages/historyPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HistoryPage = () => {
+    return <div>History Page</div>;
+};
